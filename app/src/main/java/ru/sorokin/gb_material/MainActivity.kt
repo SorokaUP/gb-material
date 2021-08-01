3073f66094e3302b7e7a9ac64aa709fd5391ee02
SorokaUP <51316654+SorokaUP@users.noreply.github.com>
2021-08-01 10:47:44 +0300

    init

---
+++ app/src/main/java/ru/sorokin/gb_material/MainActivity.kt
@@ -0,0 +1,18 @@
+package ru.sorokin.gb_material
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.sorokin.gb_material.View.PictureOfTheDayFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, PictureOfTheDayFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
